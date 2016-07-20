6ee82035607d028dc17bdab568c035a186804a04
陈晓发 <149714147@qq.com>
2016-07-20 13:07:03 +0800

    up

--- php_coroutine.h
+++ php_coroutine.h
@@ -18,14 +18,22 @@
  
 /* $Id$ */
 
+#ifndef PHP_COROUTINE_H
+#define PHP_COROUTINE_H
 
+extern zend_module_entry coroutine_module_entry;
+#define phpext_coroutine_ptr &coroutine_module_entry
 
+#define PHP_COROUTINE_VERSION "0.1.0" /* Replace with version number for your extension */
 
 #define COROUTINE_VM_STACK_INIT_SIZE 512
 
 #ifdef PHP_WIN32
+#	define PHP_COROUTINE_API __declspec(dllexport)
 #elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHP_COROUTINE_API __attribute__ ((visibility("default")))
 #else
+#	define PHP_COROUTINE_API
 #endif
 
 #ifdef ZTS
@@ -172,14 +180,19 @@ struct _coroutine_globals{
   	Declare any global variables you may need between the BEGIN
 	and END macros here:
 
+ZEND_BEGIN_MODULE_GLOBALS(coroutine)
 	zend_long  global_value;
 	char *global_string;
+ZEND_END_MODULE_GLOBALS(coroutine)
 */
 
+/* Always refer to the globals in your function as COROUTINE_G(variable).
    You are encouraged to rename these macros something shorter, see
    examples in any other php module directory.
 */
+#define COROUTINE_G(v) ZEND_MODULE_GLOBALS_ACCESSOR(coroutine, v)
 
+#if defined(ZTS) && defined(COMPILE_DL_COROUTINE)
 ZEND_TSRMLS_CACHE_EXTERN()
 #endif
 
@@ -232,6 +245,7 @@ register zend_execute_data* volatile execute_data __asm__(ZEND_VM_FP_GLOBAL_REG)
 
 
 
+#endif	/* PHP_COROUTINE_H */
 
 
 /*
